00ba791a8be3b9fcfe61b1451ed5f0a0841785d0
ian <38461291+stutterrrrer@users.noreply.github.com>
2021-09-06 13:45:06 +0800

    the provided optimal solution that uses && and || , without using any else-if .

--- part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -7,9 +7,7 @@ public class LeapYear {
         Scanner scan = new Scanner(System.in);
         System.out.println("Give a year:");
         Integer year = Integer.valueOf(scan.nextLine());
-        if (year % 4 == 0 && year % 100 != 0) {
-            System.out.println("The year is a leap year.");
-        } else if (year % 400 == 0) {
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
             System.out.println("The year is a leap year.");
         } else {
             System.out.println("The year is not a leap year.");
