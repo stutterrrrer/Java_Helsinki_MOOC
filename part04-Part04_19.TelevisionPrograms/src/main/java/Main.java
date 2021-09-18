ad1fdf2de1d25e2e2b20dfda707456d5a8939acc
ian <38461291+stutterrrrer@users.noreply.github.com>
2021-09-18 18:52:47 +0800

    the efficient way. see the diff on P04_19.TelevisionPrograms/Main

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // implement here your program that uses the TelevisionProgram class
+	public static void main(String[] args) {
+		// implement here your program that uses the TelevisionProgram class
 
-        ArrayList<TelevisionProgram> programs = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
+		ArrayList<TelevisionProgram> programs = new ArrayList<>();
+		Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Name:");
-            String name = scanner.nextLine();
-            if (name.equals("")) {
-                break;
-            }
-            System.out.println("Duration:");
-            int duration = Integer.valueOf(scanner.nextLine());
-            TelevisionProgram newProgram = new TelevisionProgram(name, duration);
-            programs.add(newProgram);
-        }
+		while (true) {
+			System.out.println("Name:");
+			String name = scanner.nextLine();
+			if (name.equals("")) {
+				break;
+			}
+			System.out.println("Duration:");
+			int duration = Integer.valueOf(scanner.nextLine());
+			// lookie here: create object and add to arraylist in one go!
+			programs.add(new TelevisionProgram(name, duration));
+		}
 
-        System.out.println("Program's maximum duration?");
-        int max = Integer.valueOf(scanner.nextLine());
-        for (TelevisionProgram program : programs) {
-            if (program.getDuration() <= max) {
-                System.out.println(program);
-            }
-        }
-    }
+		System.out.println("Program's maximum duration?");
+		int max = Integer.valueOf(scanner.nextLine());
+		for (TelevisionProgram program : programs) {
+			if (program.getDuration() <= max) {
+				System.out.println(program);
+			}
+		}
+	}
 }
