b7c1b766a7116d550039a005075af667a249d34d
ian <38461291+stutterrrrer@users.noreply.github.com>
2021-09-23 15:19:02 +0800

    improved the answer to Pt5 Ex17 based on Mooc fi's suggested solution

--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -24,29 +24,27 @@ public class Money {
 	}
 
 	public Money plus(Money addition) {
+		// didn't need to account for value of cents > 100; that exception is handled by the constructor.
 		return new Money(this.euros + addition.euros, this.cents + addition.cents);
 	}
 
 	public boolean lessThan(Money compared) {
-		return this.euros < compared.euros ||
-				(this.euros == compared.euros && this.cents < compared.cents);
+		// better solution by Mooc.fi
+		return (100 * this.euros + this.cents) < (100 * compared.euros + compared.cents);
 	}
 
 	public Money minus(Money decreaseBy) {
-		if (this.euros < decreaseBy.euros
-				|| (this.euros == decreaseBy.euros && this.cents < decreaseBy.cents)) {
-			return new Money(0, 0);
-		} else {
-			int newEuro = this.euros - decreaseBy.euros;
-			int newCents = this.cents - decreaseBy.cents;
-			// the range of newCents is [-99,99], but it should be [0,99]
-			if (newCents < 0) {
-				newCents += 100;
-				newEuro -= 1;
-			}
-			return new Money(newEuro, newCents);
+		// better solution by Mooc.fi
+		int newEuro = this.euros - decreaseBy.euros;
+		int newCents = this.cents - decreaseBy.cents;
+		// the range of newCents is [-99,99], but it should be [0,99]
+		if (newCents < 0) {
+			newCents += 100;
+			newEuro -= 1;
 		}
+		return newEuro < 0 ? new Money(0, 0) : new Money(newEuro, newCents);
 	}
+
 	public String toString() {
 		String zero = "";
 		if (this.cents < 10) {
