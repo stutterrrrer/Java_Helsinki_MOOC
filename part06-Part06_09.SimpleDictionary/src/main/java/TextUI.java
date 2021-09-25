0c512214439a46a1c23d7410a924b34cd2af039e
ian <38461291+stutterrrrer@users.noreply.github.com>
2021-09-25 21:07:29 +0800

    part6 exercise 9 - simple dictionary - TextUI class: broke down the start method into separate methods to handle different commands.

--- part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
+++ part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -18,24 +18,11 @@ public class TextUI {
 					System.out.println("Bye bye!");
 					return;
 				case "add": {
-					System.out.print("Word: ");
-					String word = scanner.nextLine();
-					System.out.print("Translation: ");
-					String translation = scanner.nextLine();
-					dictionary.add(word, translation);
+					add();
 					break;
 				}
 				case "search": {
-					System.out.print("To be translated: ");
-					String word = scanner.nextLine();
-					String translation = dictionary.translate(word);
-					// use `==` because dictionary.translate() returns String too
-					// and String is a reference type - so null is just a value of a reference variable.
-					if (translation == null) {
-						System.out.println("Word " + word + " was not found");
-					} else {
-						System.out.println("Translation: " + translation);
-					}
+					search();
 					break;
 				}
 				default:
@@ -43,5 +30,26 @@ public class TextUI {
 					break;
 			}
 		}
+	}
+
+	private void search() {
+		System.out.print("To be translated: ");
+		String word = scanner.nextLine();
+		String translation = dictionary.translate(word);
+		// use `==` because dictionary.translate() returns String too
+		// and String is a reference type - so null is just a value of a reference variable.
+		if (translation == null) {
+			System.out.println("Word " + word + " was not found");
+		} else {
+			System.out.println("Translation: " + translation);
+		}
+	}
+
+	private void add() {
+		System.out.print("Word: ");
+		String word = scanner.nextLine();
+		System.out.print("Translation: ");
+		String translation = scanner.nextLine();
+		dictionary.add(word, translation);
 	}
 }
